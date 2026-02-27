80cb66fcfe13e4f547bc143b327a377b8d9806ec
xorchi <jperkasa8@gmail.com>
2026-02-28 06:33:24 +0700

    chore: set versionName to 1.0.4-nightly for dev branch

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
         minSdk = 26
         targetSdk = 35
         versionCode = 4
-        versionName = "1.0.2"
+        versionName = "1.0.4-nightly"
         packaging {
             jniLibs {
                 useLegacyPackaging = true
